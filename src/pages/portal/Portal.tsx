1840494b81f3b12524edc8f7cc80831bc8a7c815
Phil-GCN <hello@theglobalcitizen.info>
2026-03-29 09:47:21 +0200

    Refactor Portal component and add OrderDetails route

--- src/pages/portal/Portal.tsx
+++ src/pages/portal/Portal.tsx
@@ -10,6 +10,7 @@ import ProductsManager from './ProductsManager';
 import ContentManager from './ContentManager';
 import CommunityManager from './CommunityManager';
 import OrdersManager from './OrdersManager';
+import OrderDetails from './OrderDetails';
 import Analytics from './Analytics';
 import SettingsManager from '../admin/Settings';
 
@@ -24,21 +25,22 @@ const Portal: React.FC = () => {
     );
   }
 
-  return (
-    <PortalLayout>
+  function Portal() {
+    return (
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/users" element={<UsersManager />} />
-        <Route path="/products" element={<ProductsManager />} />
-        <Route path="/content" element={<ContentManager />} />
-        <Route path="/community" element={<CommunityManager />} />
-        <Route path="/orders" element={<OrdersManager />} />
-        <Route path="/analytics" element={<Analytics />} />
-        <Route path="/settings" element={<SettingsManager />} />
-        <Route path="*" element={<Navigate to="/portal" replace />} />
+        <Route path="/" element={<PortalLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="users" element={<UsersManager />} />
+          <Route path="products" element={<ProductsManager />} />
+          <Route path="orders" element={<OrdersManager />} />
+          <Route path="orders/:id" element={<OrderDetails />} />  {/* ADD THIS */}
+          <Route path="content" element={<ContentManager />} />
+          <Route path="community" element={<Community />} />
+          <Route path="analytics" element={<Analytics />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
       </Routes>
-    </PortalLayout>
-  );
-};
+    );
+  }
 
 export default Portal;
